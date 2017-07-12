087ee46389c87896a95879825baa9043966ae60f
Nathan Grove <ngrove@exegy.com>
2017-07-12 09:40:58 -0500

    added encryption and decryption

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const app = express()
+const app = require('express')();
+var bodyParser = require('body-parser')
+var crypto = require('crypto');
+let algorithm = 'aes256';
+var UglifyJS = require("uglify-js");
 
-app.get('/run', function (req, res) {
+let keys = {};
 
+app.use(bodyParser.json());
+
+app.post('/encrypt', function(req,res){
+
+  // wrap the code in commonjs module and then uglify it.
+  let code = UglifyJS.minify('module.exports = function(request,response){' + req.body.code + '};').code;
+
+  // generate a crypto key
+  let key = crypto.createHash('md5').update(code + 'saltedWithSaltySalt' ).digest('hex');
+
+  // init the cipher
+  var cipher = crypto.createCipher(algorithm, key);  
+
+  // encrypt the code
+  let encrypted = cipher.update(code, 'utf8', 'hex') + cipher.final('hex');
+
+  // store the encryption key
+  keys[crypto.createHash('md5').update(encrypted).digest('hex')] = key;
+
+  // send the encrypted code
+  res.send(encrypted);
+})
+
+app.post('/run', function (req, res) {
+
+  // create a new VM
   const {NodeVM} = require('vm2');
-
   const vm = new NodeVM({
     console: 'redirect',
     sandbox: {},
@@ -13,27 +41,34 @@ app.get('/run', function (req, res) {
       root: './vm/node_modules'
     }
   });
+    
+  // get the encrypted code
+  let encryptedCode = req.body.code;
 
-  let executionCode = `
-    var request = require('./vm/node_modules/request');
-    request(vars.url, function (error, response, body) {
-      console.error(error);
-      if (!error && response.statusCode == 200) {
-        callback(body); // Show the HTML for the Google homepage.
-      }
-    })`;
+  // get the decryption key
+  let key = keys[crypto.createHash('md5').update(encryptedCode).digest('hex')];
+  if (!key) {
+    res.status(500);
+    return res.send('Invalid code');
+  }
 
-  let runFunc = vm.run(`
-    module.exports = function(vars,callback){
-      ${executionCode}
-    };
-  `,'vm.js');
+  // convert key into buffer
+  key = Buffer.from(key,'utf8');
 
-  runFunc({ url: 'https://nathangrove.com' }, output => {
-    res.send(output);
-  });
+  // create the decipher
+  var decipher = crypto.createDecipher(algorithm, key);
+
+  // decrypt
+  var executionCode = decipher.update(encryptedCode, 'hex', 'utf8') + decipher.final('utf8');
+
+  // prepare the vm
+  let runFunc = vm.run(executionCode,'vm.js');
+
+  // run the code
+  runFunc(req, res);
 
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# encrypt some simple http proxy code
+ENCRYPTED=$(curl -X POST \
+  http://localhost:3000/encrypt \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -s \
+  -d '{
+  "code": "if (!request.query.url) return response.send('\''A URL is requried as a query parameter'\''); var requestModule = require('\''./vm/node_modules/request'\''); requestModule(request.query.url, function (error, res, body) { if (!error && res.statusCode == 200) { response.send(body); }})"
+}')
+
+# execute the code
+echo $(curl -X POST \
+  'http://localhost:3000/run?url=https%3A%2F%2Fnathangrove.com' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -s \
+  -d "{
+  \"code\": \"$ENCRYPTED\"
+}")
+
