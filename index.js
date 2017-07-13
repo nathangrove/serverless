00aa3e217640b68556cc9c57e0f9882e5eb0fe42
Nathan Grove <nathan@nathangrove.com>
2017-07-12 22:24:05 -0500

    implemented JWT authorization

--- index.js
+++ index.js
@@ -14,21 +14,48 @@ users.persistence.setAutocompactionInterval(1800000);
 keys.persistence.setAutocompactionInterval(60000);
 users.ensureIndex({ fieldName : 'username', unique: true }, err => { if(err) console.log(err); });
 
-app.prototype.authenticate = function(request,callback){
 
+// an authentication function
+app.authenticate = function(request,callback){
 
-  var authHeader = req.headers['authorization'].trim().split(/\s+/);
+  var authHeader = request.headers['authorization'].trim().split(/\s+/);
+
+  // was it a bearer (jwt) token?
   if (authHeader[0] == 'Bearer'){
-    let auth = authHeader[1].split('.');
-    // TODO: Implement Bearer token check.
-    // verify signature
-    // verify payload
-    
+    let auth = authHeader[1].split('.').map( str => new Buffer(str,'base64').toString() );
+
+    // get the parts of the jwt
+    let header = auth[0];
+    let payload = JSON.parse(auth[1]);
+    let signature = auth[2];
+
+    console.log(signature);
+    // find the user by username in the payload
+    users.findOne({ username: payload.user }, (error, user) => { 
+      if (!user || !user.active) return callback("Invalid user");
+      if (signature != encodeURI(crypto.createHmac('SHA256',user._id + "saltedWithSuperSaltySalt").update(`${header}.${JSON.stringify(payload)}`).digest('hex')))
+        return callback('Invalid signature');
+
+      // convert key into buffer
+      key = Buffer.from(user._id,'utf8');
+      var decipher = crypto.createDecipher(algorithm, key);
+      var profile = JSON.parse(decipher.update(payload.data, 'hex', 'utf8') + decipher.final('utf8'));
+
+      // if the payload was modified...then its wrong...
+      if (profile.user != user._id) return callback('Invalid user');
+
+      // if we made it here..then it checks out...
+      callback(null,user);
+
+    });
+  
+  // perhaps basic auth? 
   } else if (authHeader[0] == 'Basic'){
     let auth = new Buffer(authHeader[1], 'base64').toString().split(/:/);
     let password = crypto.createHash('md5').update(auth[1]).digest('hex') 
-    users.findOne({ username: auth[0], password: password }, (err, user) => callback(user) );
+    users.findOne({ username: auth[0], password: password }, (err, user) => callback(null,user) );
   }
+
 }
 
 // serve up the public directory
@@ -39,6 +66,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 
 
+// return a JWT
 app.post('/login', function(req,res){
   let password = crypto.createHash('md5').update(req.body.password).digest('hex');
   users.findOne({ username: req.body.username, password: password }, (error, user) => {
@@ -48,8 +76,7 @@ app.post('/login', function(req,res){
     }
 
     // build the jwt
-    // jwt header
-    let header = encodeURI(new Buffer(JSON.stringify({ "alg": "HS256", "typ": "JWT" })).toString('base64'));
+    let header = JSON.stringify({ "alg": "HS256", "typ": "JWT" });
 
     // jwt payload
     var cipher = crypto.createCipher(algorithm, user._id);
@@ -58,54 +85,63 @@ app.post('/login', function(req,res){
       ip: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
       user: user._id
     }), 'utf8', 'hex') + cipher.final('hex');
-
-    let payload = encodeURI(new Buffer(JSON.stringify({ "user": user.username, "data": encryptedPayload })).toString('base64'));
+    let payload = JSON.stringify({ "user": user.username, "data": encryptedPayload });
 
     // jwt signature
-    let signature = encodeURI(crypto.createHmac('SHA256',user._id + "saltedWithSuperSaltySalt").update(`${header}.${payload}`).digest('base64'));
-    res.send({ token: `${header}.${payload}.${signature}`});
+    let signature = crypto.createHmac('SHA256',user._id + "saltedWithSuperSaltySalt").update(`${header}.${payload}`).digest('hex');
+    console.log(signature);
 
-    // return the bearer token
-    res.send({ token: token });
+    // prepare for http transportation
+    header = encodeURI(new Buffer(header).toString('base64'));
+    payload = encodeURI(new Buffer(payload).toString('base64'));
+    signature = encodeURI(new Buffer(signature).toString('base64'));
+
+    // return the bearer token 
+    res.send({ token: `${header}.${payload}.${signature}`});
 
   });
 
 })
 
+
+// encrpyt code
 app.post('/encrypt', function(req,res){
 
   // authenticate the user
-  let user = app.authenticate(req);
-  if (!user) {
-    res.status(403);
-    return res.send("<h1>403</h1><h2>Unauthorized</h2>");
-  }
+  app.authenticate(req, (error,user) => {
 
-  // wrap the code in commonjs module and then uglify it.
-  let code = UglifyJS.minify('module.exports = function(request,response){' + req.body.code + '};').code;
+    if (!user) {
+      res.status(403);
+      return res.send(`<h1>403</h1><h2>Unauthorized: ${error}</h2>`);
+    }
 
-  // generate a crypto key
-  let salt = Math.random() + 'saltingTheSaltWithSaltySalt';
-  let key = crypto.createHash('md5').update(code + salt).digest('hex');
+    // wrap the code in commonjs module and then uglify it.
+    let code = UglifyJS.minify('module.exports = function(request,response){' + req.body.code + '};').code;
 
-  // init the cipher
-  var cipher = crypto.createCipher(algorithm, key);
+    // generate a crypto key
+    let salt = Math.random() + 'saltingTheSaltWithSaltySalt';
+    let key = crypto.createHash('md5').update(code + salt).digest('hex');
 
-  // encrypt the code
-  let encrypted = cipher.update(code, 'utf8', 'hex') + cipher.final('hex');
+    // init the cipher
+    var cipher = crypto.createCipher(algorithm, key);
 
-  // store the encryption key with the id being the md5 of the encrypted code
-  keys.insert({
-    _id: crypto.createHash('md5').update(encrypted).digest('hex'),
-    user: user._id,
-    runs: 0,
-    last: 0,
-    created: new Date().getTime(),
-    key: key
+    // encrypt the code
+    let encrypted = cipher.update(code, 'utf8', 'hex') + cipher.final('hex');
+
+    // store the encryption key with the id being the md5 of the encrypted code
+    keys.insert({
+      _id: crypto.createHash('md5').update(encrypted).digest('hex'),
+      user: user._id,
+      runs: 0,
+      last: 0,
+      created: new Date().getTime(),
+      key: key
+    });
+
+    // send the encrypted code
+    res.send(encrypted);
+
   });
-
-  // send the encrypted code
-  res.send(encrypted);
 
 })
 
